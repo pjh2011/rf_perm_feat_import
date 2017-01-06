5d4acb3cba114ce260f5ac95505287b5ff957504
Peter Hughes <ptrjhghs@gmail.com>
2017-01-06 12:51:16 -0800

    changed setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='sklearn_perm_feat_import',
       version='0.1',
       description='Random Forest Feature Importance on OOB Score Delta',
-      url='https://github.com/pjh2011/sklearn_oob_feat_import',
+      url='https://github.com/pjh2011/sklearn_perm_feat_import',
       author='Peter Hughes',
       author_email='pethug210@gmail.com',
       license='MIT',
